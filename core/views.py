a7d6579d3e9e9c7e28006718fe795c4d1d4d81b0
JanMinarcin <150118153+JanMinarcin@users.noreply.github.com>
2025-02-11 12:54:18 +0100

    Registracia/Prihlasovanie

--- core/views.py
+++ core/views.py
@@ -1,6 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 # Create your views here.
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+       Meno = request.POST['Meno']
+       email = request.POST['email']
+       Heslo = request.POST['Heslo']
+       Heslo2 = request.POST['Heslo2']
+
+       if Heslo == Heslo2:
+           if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email je už použitý')
+                return redirect('signup')
+           elif User.objects.filter(username=Meno).exists():
+                messages.info(request, 'Meno je už použité')
+                return redirect('signup')
+           else:
+                user = User.objects.create_user(username=Meno, email=email, password=Heslo)
+                user.save()
+                
+
+
+                user_model = User.objects.get(username=Meno)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
+       else:
+              messages.info(request, 'Heslá sa nezhodujú')
+              return redirect('signup')
+           
+       
+    else:
+        return render(request, 'signup.html')
+    
+def signin(request):
+    if request.method == 'POST':
+        Meno = request.POST['Meno']
+        Heslo = request.POST['Heslo']
+
+        user = auth.authenticate(username=Meno, password=Heslo)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Nesprávne meno alebo heslo')
+            return redirect('signin')
+    else:
+        return render(request, 'signin.html')
+    
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
